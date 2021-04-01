e6da93424d0950999c9c371adc5d7890e12af9e0
Cristiam Sosa <cristiam.sosa@innocv.com>
2021-04-01 03:58:12 +0200

    Update docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=Iu3TLLKg31L0XgL1hksgF4nZxsZkNmN5`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=Iu3TLLKg31L0XgL1hksgF4nZxsZkNmN5`;
   const response = await fetch(url);
   const {data} = await response.json();
 
